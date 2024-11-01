8d99dc971c62e2f7dd4512976e6ad47a06804874
Max Thomson <git@mnt.dev>
2024-11-01 13:44:39 -0700

    Add default tracing-subscriber

--- div176/src/main.rs
+++ div176/src/main.rs
@@ -1,13 +1,24 @@
 use axum::{routing::get, Router};
+use telemetry::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,axum::rejection=trace", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new().route("/", get("div176"));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
 
-    println!("listening on {}", listener.local_addr().unwrap());
+    info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
