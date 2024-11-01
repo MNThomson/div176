8990d62ba2507475435c902c8948ffad4dd72a0f
Max Thomson <git@mnt.dev>
2024-11-01 12:34:27 -0700

    Initialize Axum server

--- div176/src/main.rs
+++ div176/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get("div176"));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
