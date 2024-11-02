60294f86bbfb052b7e2601a7d2687913e780f1ae
Max Thomson <git@mnt.dev>
2024-11-02 11:47:14 -0700

    Otel HTTP server span attributes

--- crates/telemetry/src/lib.rs
+++ crates/telemetry/src/lib.rs
@@ -1,9 +1,9 @@
 use std::time::Duration;
 
-use axum::response::Response;
+use axum::{extract::MatchedPath, response::Response};
 use gethostname::gethostname;
-use http::Request;
-use opentelemetry::KeyValue;
+use http::{header, Request, Version};
+use opentelemetry::{trace::SpanKind, KeyValue};
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::{runtime, trace as sdktrace, Resource};
 use rustc_version::version;
@@ -109,8 +109,33 @@ pub fn otel_tracing() -> TraceLayer<
 #[derive(Clone, Copy, Debug)]
 pub struct OtelMakeSpan;
 impl<B> MakeSpan<B> for OtelMakeSpan {
-    fn make_span(&mut self, _req: &Request<B>) -> Span {
-        tracing::info_span!("http_request")
+    fn make_span(&mut self, request: &Request<B>) -> Span {
+        let matched_route = request
+            .extensions()
+            .get::<MatchedPath>()
+            .map(MatchedPath::as_str);
+
+        tracing::info_span!(
+            "http_request",
+            "otel.name" = format!("{} {}", request.method(), matched_route.unwrap_or("UnknownRoute")),
+            "otel.kind" = format!("{:?}", SpanKind::Server),
+            "http.request.method" = ?request.method(),
+            "http.route" = matched_route,
+            "url.path" = request.uri().path(),
+            "url.query" = request.uri().query(),
+            "user_agent.original" = request.headers().get(header::USER_AGENT).and_then(|val| val.to_str().ok()),
+            "http.flavor" = match request.version() {
+                Version::HTTP_09 => "0.9",
+                Version::HTTP_10 => "1.0",
+                Version::HTTP_11 => "1.1",
+                Version::HTTP_2 => "2.0",
+                Version::HTTP_3 => "3.0",
+                _ => "Unknown",
+            },
+            "http.request.content_length" = request.headers().get(header::CONTENT_LENGTH).and_then(|val| val.to_str().ok()),
+            "http.response.status_code" = tracing::field::Empty,
+            "error.type" = tracing::field::Empty,
+        )
     }
 }
 
@@ -123,8 +148,15 @@ impl<B> OnRequest<B> for OtelOnRequest {
 #[derive(Clone, Copy, Debug)]
 pub struct OtelOnResponse;
 impl<B> OnResponse<B> for OtelOnResponse {
-    fn on_response(self, _: &Response<B>, _: Duration, _: &Span) {
-        info!("");
+    fn on_response(self, response: &Response<B>, _: Duration, span: &Span) {
+        span.record(
+            "http.response.status_code",
+            response.status().as_u16() as i64,
+        );
+        if response.status().is_server_error() {
+            span.record("otel.status_code", "ERROR");
+        }
+        debug!("Request served");
     }
 }
 
@@ -143,5 +175,9 @@ impl OnEos for OtelOnEos {
 #[derive(Clone, Copy, Debug)]
 pub struct OtelOnFailure;
 impl OnFailure<ServerErrorsFailureClass> for OtelOnFailure {
-    fn on_failure(&mut self, _: ServerErrorsFailureClass, _: Duration, _: &Span) {}
+    fn on_failure(&mut self, error: ServerErrorsFailureClass, _: Duration, span: &Span) {
+        span.record("otel.status_code", "ERROR");
+        span.record("error.type", error.to_string());
+        debug!("Request errored");
+    }
 }
