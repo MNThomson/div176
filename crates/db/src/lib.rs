d064ff83d0baefc71dd72ba0c448424c60c21069
Max Thomson <git@mnt.dev>
2024-11-03 11:26:28 -0800

    Add unreachable_macro for trait implementation

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -1,12 +1,15 @@
+use unreachable_macro::with_unreachable_defaults;
+
+#[with_unreachable_defaults]
 pub trait Database {
-    fn healthcheck(self) -> Result<(), ()>;
+    fn healthcheck(&self) -> Result<(), ()>;
 }
 
 #[derive(Clone)]
 pub struct DB {}
 
 impl Database for DB {
-    fn healthcheck(self) -> Result<(), ()> {
+    fn healthcheck(&self) -> Result<(), ()> {
         Ok(())
     }
 }
---
+++ crates/unreachable_macro/src/lib.rs
@@ -0,0 +1,35 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemTrait, TraitItem, TraitItemFn};
+
+#[proc_macro_attribute]
+pub fn with_unreachable_defaults(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    if !cfg!(test) {
+        return item;
+    }
+
+    let input = parse_macro_input!(item as ItemTrait);
+
+    let trait_name = &input.ident;
+    let trait_vis = &input.vis;
+    let trait_methods = input.items.iter().filter_map(|item| {
+        if let TraitItem::Fn(TraitItemFn { sig, .. }) = item {
+            let method_sig = &sig;
+            Some(quote! {
+                #method_sig {
+                    unreachable!();
+                }
+            })
+        } else {
+            None
+        }
+    });
+
+    let generated_trait = quote! {
+        #trait_vis trait #trait_name {
+            #(#trait_methods)*
+        }
+    };
+
+    TokenStream::from(generated_trait)
+}
