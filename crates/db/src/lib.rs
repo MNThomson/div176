1cd5ee75f686315f9e3ca80544a741a1abc80a4e
Max Thomson <git@mnt.dev>
2024-11-06 16:20:24 -0800

    Init db options

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -1,19 +1,51 @@
-use anyhow::Result;
-use sqlx::{Sqlite, SqlitePool};
+#![feature(associated_type_defaults)]
+#![allow(async_fn_in_trait)]
+
+use std::{fs::File, time::Duration};
+
+use anyhow::{bail, Context, Result};
+use sqlx::{sqlite::SqlitePoolOptions, Sqlite, SqlitePool};
 
 pub type DbType = Sqlite;
 pub type DbPool = SqlitePool;
 
 #[cfg_attr(test, unreachable_macro::with_unreachable_defaults)]
 pub trait Database {
-    fn healthcheck(&self) -> Result<()>;
+    async fn healthcheck(&self) -> Result<()>;
 }
 
 #[derive(Clone)]
-pub struct DB {}
+pub struct DB {
+    pool: DbPool,
+}
+
+const PATH: &str = "data.db";
+
+impl DB {
+    pub async fn init() -> Result<Self> {
+        File::open(PATH).or_else(|_| File::create(PATH)).unwrap();
+        let db = DB {
+            pool: SqlitePoolOptions::new()
+                .max_connections(50)
+                .acquire_timeout(Duration::from_secs(3))
+                .connect(format!("sqlite://{}", PATH).as_str())
+                .await
+                .context("Could not connect to database (with URL)")?,
+        };
+        Ok(db)
+    }
+}
 
 impl Database for DB {
-    fn healthcheck(&self) -> Result<()> {
-        Ok(())
+    async fn healthcheck(&self) -> Result<()> {
+        let row: (i64,) = sqlx::query_as("SELECT 1")
+            .fetch_one(&self.pool)
+            .await
+            .unwrap_or((0,));
+        if row.0 == 1 {
+            Ok(())
+        } else {
+            bail!("not healthy")
+        }
     }
 }
--- div176/src/main.rs
+++ div176/src/main.rs
@@ -13,7 +13,9 @@ struct AppState {
 async fn main() {
     tracing_init(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
-    let state = AppState { db: DB {} };
+    let state = AppState {
+        db: DB::init().await.unwrap(),
+    };
 
     let app = Router::new()
         .route("/", get(rsx!(<h1>div176</h1>).render()))
@@ -30,10 +32,13 @@ async fn main() {
 }
 
 async fn healthcheck(State(state): State<AppState>) -> impl IntoResponse {
-    let health = state.db.healthcheck().map_or("unhealthy", |_| "healthy");
-    let statuscode = state
-        .db
-        .healthcheck()
-        .map_or(StatusCode::INTERNAL_SERVER_ERROR, |_| StatusCode::OK);
-    (statuscode, rsx!(<p>database: {health}</p>).render())
+    let is_healthy = state.db.healthcheck().await.is_ok();
+    let health_status = if is_healthy { "healthy" } else { "unhealthy" };
+    let statuscode = if is_healthy {
+        StatusCode::OK
+    } else {
+        StatusCode::INTERNAL_SERVER_ERROR
+    };
+
+    (statuscode, rsx!(<p>database: {health_status}</p>).render())
 }
