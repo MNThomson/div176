8e7fa7120eedae057dfdebff0780f6cb5a5ec157
Max Thomson <git@mnt.dev>
2024-12-31 18:03:37 -0800

    Add tailwind to base layout

--- crates/components/src/lib.rs
+++ crates/components/src/lib.rs
@@ -11,6 +11,8 @@ pub fn Layout(inner: impl Renderable) -> impl Renderable {
                 <meta name="darkreader-lock">
                 <link rel="icon" href="/static/img/favicon.svg">
                 <title>Div176</title>
+                <script src="https://cdn.tailwindcss.com/3.4.15"></script>
+                <script>r#"tailwind.config = {}"#</script>
             </head>
             <body>
                 { inner }
