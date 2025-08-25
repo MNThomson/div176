69390d316ca38151ae50a681f062dd74c1375a79
Max Thomson <git@mnt.dev>
2025-08-24 23:40:07 -0700

    Create Error type

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,1 +1,78 @@
 mod type_alias;
+
+use std::{borrow::Cow, collections::HashMap};
+
+use axum::{
+    Json,
+    body::Body,
+    http::{Response, StatusCode},
+    response::IntoResponse,
+};
+use tracing::error;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("authentication required")]
+    Unauthorized,
+    #[error("user may not perform that action")]
+    Forbidden,
+    #[error("request path not found")]
+    NotFound,
+    #[error("error in the request body")]
+    UnprocessableEntity {
+        errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+    },
+    #[error("an error occurred with the database")]
+    Sqlx(#[from] sqlx::Error),
+    #[error("an internal server error occurred")]
+    Anyhow(#[from] anyhow::Error),
+}
+
+impl Error {
+    pub fn unprocessable_entity<K, V>(errors: impl IntoIterator<Item = (K, V)>) -> Self
+    where
+        K: Into<Cow<'static, str>>,
+        V: Into<Cow<'static, str>>,
+    {
+        let mut error_map = HashMap::new();
+
+        for (key, val) in errors {
+            error_map
+                .entry(key.into())
+                .or_insert_with(Vec::new)
+                .push(val.into());
+        }
+
+        Self::UnprocessableEntity { errors: error_map }
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            Self::Unauthorized => StatusCode::UNAUTHORIZED,
+            Self::Forbidden => StatusCode::FORBIDDEN,
+            Self::NotFound => StatusCode::NOT_FOUND,
+            Self::UnprocessableEntity { .. } => StatusCode::UNPROCESSABLE_ENTITY,
+            Self::Sqlx(_) | Self::Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response<Body> {
+        match self {
+            Self::UnprocessableEntity { errors } => {
+                #[derive(serde::Serialize)]
+                struct Errors {
+                    errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+                }
+
+                return (StatusCode::UNPROCESSABLE_ENTITY, Json(Errors { errors })).into_response();
+            }
+            Self::Sqlx(ref e) => error!("SQLx error: {:?}", e),
+            Self::Anyhow(ref e) => error!("Generic error: {:?}", e),
+            _ => (),
+        }
+
+        (self.status_code(), self.to_string()).into_response()
+    }
+}
